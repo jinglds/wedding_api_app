889c700cdc500d89f33431427ef2595079d4a986
Jing <ladasa.tira@gmail.com>
2014-10-21 13:25:18 +0700

    before go back

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,4 +11,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+          # def facebook
+          #     user = User.from_omniauth(request.env["omniauth.auth"])
+          #   if user.persisted?
+                    
+          #     sign_in_and_redirect user
+          #   else
+          #     session["devise.user_attributes"] = user.attributes
+          #     redirect_to new_user_registration_url
+          #   end
+          # end
 end
